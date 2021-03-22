55a9d4d7ea602bb88123d87a9ec4ae287909629e
Pedro Lucas <pedronun@protonmail.com>
2021-03-22 16:59:39 -0300

    Moved content from App.tsx to Components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,49 @@
-export function Content() {
-  // Complete aqui
+import { useEffect } from "react";
+import { useState } from "react";
+import { api } from "../services/api";
+import { MovieCard } from "./MovieCard";
+
+import "../styles/content.scss";
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  selecteGenreId: number;
+}
+
+export function Content(props: ContentProps) {
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api
+      .get<MovieProps[]>(`movies/?Genre_id=${props.selecteGenreId}`)
+      .then((response) => {
+        setMovies(response.data);
+      });
+  }, [props.selecteGenreId]);
+
+  return (
+    <main>
+      <div className="movies-list">
+        {movies.map((movie) => (
+          <MovieCard
+            key={movie.imdbID}
+            title={movie.Title}
+            poster={movie.Poster}
+            runtime={movie.Runtime}
+            rating={movie.Ratings[0].Value}
+          />
+        ))}
+      </div>
+    </main>
+  );
 }
